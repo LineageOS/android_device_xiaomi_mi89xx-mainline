4f6e8edc119c3db1b68d9ddae9585bfc7c2faeb9
Yumi Yukimura <me.cafebabe@gmail.com>
2025-04-13 19:18:46 +0800

    [DNM] mi89xx-mainline: daisy_mainline draft
    Change-Id: Ic6055b46f7d8290396e5d715cbafe09df9bf83a5

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -4,9 +4,11 @@
 #
 
 PRODUCT_MAKEFILES := \
+    lineage_daisy_mainline:$(LOCAL_DIR)/daisy_mainline/lineage_daisy_mainline.mk \
     lineage_mi8953_a:$(LOCAL_DIR)/mi8953_a/lineage_mi8953_a.mk \
     lineage_mi89x7:$(LOCAL_DIR)/mi89x7/lineage_mi89x7.mk
 
 $(foreach build_type, user userdebug eng, \
+    $(eval COMMON_LUNCH_CHOICES += lineage_daisy_mainline-$(build_type)) \
     $(eval COMMON_LUNCH_CHOICES += lineage_mi8953_a-$(build_type)) \
     $(eval COMMON_LUNCH_CHOICES += lineage_mi89x7-$(build_type)))
