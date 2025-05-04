8282dd3f1a2befd883d4d09d31fd763153c2b8c9
Yumi Yukimura <me.cafebabe@gmail.com>
2025-05-04 17:55:34 +0800

    mi89xx-mainline: mkbootimg: Add missing bootimage-to-kernel call
    Change-Id: Ia91544f7e6fb3b528fbb42ea8a45572d9c73b881

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -23,6 +23,8 @@ define build-lk2nd-boot-image
 	cat $(2) >> $(1)
 endef
 
+$(foreach b,$(INSTALLED_BOOTIMAGE_TARGET), $(eval $(call add-dependency,$(b),$(call bootimage-to-kernel,$(b)))))
+
 $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(BOOTIMAGE_EXTRA_DEPS) $(MKBOOTIMG_LK2ND_IMAGE_PATH)
 	$(call pretty,"Target boot image with lk2nd: $@")
 	$(MKBOOTIMG) --kernel $(call bootimage-to-kernel,$@) $(INTERNAL_BOOTIMAGE_ARGS) $(INTERNAL_MKBOOTIMG_VERSION_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@.mkbootimg
