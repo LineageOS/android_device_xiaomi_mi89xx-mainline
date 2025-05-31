e88a23905d88595ad6602b0d093c7819f46e4aa9
Yumi Yukimura <me.cafebabe@gmail.com>
2025-06-01 05:16:14 +0800

    mi89xx-mainline: Add initial `mi8916` target
    Change-Id: Ic88258af4e235103fcd61655b4f22f609465267b

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -4,9 +4,11 @@
 #
 
 PRODUCT_MAKEFILES := \
+    lineage_mi8916:$(LOCAL_DIR)/mi8916/lineage_mi8916.mk \
     lineage_mi8953_a:$(LOCAL_DIR)/mi8953_a/lineage_mi8953_a.mk \
     lineage_mi89x7:$(LOCAL_DIR)/mi89x7/lineage_mi89x7.mk
 
 $(foreach build_type, user userdebug eng, \
+    $(eval COMMON_LUNCH_CHOICES += lineage_mi8916-$(build_type)) \
     $(eval COMMON_LUNCH_CHOICES += lineage_mi8953_a-$(build_type)) \
     $(eval COMMON_LUNCH_CHOICES += lineage_mi89x7-$(build_type)))
