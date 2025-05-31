47faed0710123a84fa8f621241c7bfff038ee824
Yumi Yukimura <me.cafebabe@gmail.com>
2025-06-01 05:16:14 +0800

    mi89xx-mainline: Add initial `mi8916` target
    Change-Id: Ic88258af4e235103fcd61655b4f22f609465267b

---
+++ mi8916/BoardConfig.mk
@@ -0,0 +1,65 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from parent
+include device/xiaomi/mi89xx-mainline/BoardConfig.mk
+
+# Boot parameters
+BOARD_KERNEL_CMDLINE += \
+    androidboot.hardware=mi8916
+
+# Kernel
+TARGET_KERNEL_SOURCE := kernel/xiaomi/mi8916-mainline
+
+TARGET_DTB_LIST_WILDCARD := \
+    qcom/msm8916-wingtech-wt88047
+
+TARGET_KERNEL_CONFIG := \
+    msm8916_defconfig
+
+TARGET_KERNEL_CONFIG_EXT := \
+    kernel/mainline/configs/fragments/y/arm64/gki_pre.config \
+    kernel/mainline/configs/fragments/y/arm64/gki.config \
+    kernel/mainline/configs/fragments/y/arm64/gki_post.config \
+    kernel/mainline/configs/fragments/y/common.config \
+    kernel/mainline/configs/fragments/y/fbcon.config \
+    kernel/mainline/configs/fragments/n/disable-clang-hardening-features.config \
+    kernel/mainline/configs/fragments/n/faster-build-time.config
+
+# Kernel modules
+BOARD_VENDOR_KERNEL_MODULES_LOAD := \
+    $(strip $(shell cat $(TARGET_DEVICE_PATH)/modprobe/modules.load.basic)) \
+    $(strip $(shell cat $(TARGET_DEVICE_PATH)/modprobe/modules.load.drm)) \
+    $(strip $(shell cat $(TARGET_DEVICE_PATH)/modprobe/modules.load.touchscreen))
+BOARD_RECOVERY_RAMDISK_KERNEL_MODULES_LOAD := \
+    $(strip $(shell cat $(TARGET_DEVICE_PATH)/modprobe/modules.load.basic)) \
+    $(strip $(shell cat $(TARGET_DEVICE_PATH)/modprobe/modules.load.drm)) \
+    $(strip $(shell cat $(TARGET_DEVICE_PATH)/modprobe/modules.load.touchscreen))
+RECOVERY_KERNEL_MODULES := \
+    $(strip $(shell cat $(TARGET_DEVICE_PATH)/modprobe/modules.include_dep.basic)) \
+    $(strip $(shell cat $(TARGET_DEVICE_PATH)/modprobe/modules.include_dep.drm)) \
+    $(strip $(shell cat $(TARGET_DEVICE_PATH)/modprobe/modules.load.basic)) \
+    $(strip $(shell cat $(TARGET_DEVICE_PATH)/modprobe/modules.load.drm)) \
+    $(strip $(shell cat $(TARGET_DEVICE_PATH)/modprobe/modules.load.touchscreen))
+SYSTEM_KERNEL_MODULES := \
+    $(strip $(shell cat $(TARGET_DEVICE_PATH)/modprobe/modules.include.system))
+
+# OTA
+TARGET_OTA_ASSERT_DEVICE := mi8916
+
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 33554432
+BOARD_CACHEIMAGE_PARTITION_SIZE := 335544320
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 33554432
+BOARD_SYSTEMIMAGE_EXTFS_INODE_COUNT := -1
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1073741824
+
+# Properties
+TARGET_ODM_PROP += $(TARGET_DEVICE_PATH)/properties/odm.prop
+
+# Recovery
+TARGET_RECOVERY_DENSITY := hdpi
+TARGET_RECOVERY_FSTAB := $(TARGET_DEVICE_PATH)/fstab/fstab.mi8916
---
+++ mi8916/device.mk
@@ -0,0 +1,47 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+TARGET_DEVICE_PATH := device/xiaomi/mi89xx-mainline/mi8916
+
+# Inherit options from mainline/qcom-common
+TARGET_QCOM_SOC_FAMILY := msm8916
+## TODO: Bringup the corresponding hardware and remove the following definitions
+TARGET_AUDIO_HAL := default-aidl
+TARGET_SUPPORTS_SUSPEND := false
+include device/mainline/qcom-common/optional/options.mk
+
+# Inherit from parent
+## A/B
+AB_OTA_UPDATER := false
+$(call inherit-product, device/xiaomi/mi89xx-mainline/device.mk)
+
+# Boot animation
+TARGET_BOOTANIMATION_HALF_RES := true
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+# Dalvik heap
+$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
+
+# Init
+PRODUCT_COPY_FILES += \
+    $(TARGET_DEVICE_PATH)/fstab/fstab.mi8916:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.mi8916 \
+    $(TARGET_DEVICE_PATH)/init/init.mi8916.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.mi8916.rc
+
+PRODUCT_PACKAGES += \
+    use_memfd.rc \
+    zram.rc
+
+# Ramdisk
+PRODUCT_COPY_FILES += \
+    $(TARGET_DEVICE_PATH)/fstab/fstab.mi8916:$(TARGET_COPY_OUT_RAMDISK)/fstab.mi8916
+
+# Recovery
+PRODUCT_COPY_FILES += \
+    $(TARGET_DEVICE_PATH)/init/init.recovery.mi8916.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.mi8916.rc
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    kernel/mainline/configs
