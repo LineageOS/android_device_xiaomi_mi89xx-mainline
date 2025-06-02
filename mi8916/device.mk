2e61d58e9bcc4a6394660af95daff15d7ff3a66b
Yumi Yukimura <me.cafebabe@gmail.com>
2025-06-03 00:01:16 +0800

    mi89xx-mainline: mi8916: Setup firmware symlinks
    Change-Id: I5d1bda0f1e460df5cd74bde4eb3da5d98c647142

--- mi8916/device.mk
+++ mi8916/device.mk
@@ -25,6 +25,10 @@ TARGET_SCREEN_WIDTH := 720
 # Dalvik heap
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 
+# Firmware
+PRODUCT_PACKAGES += \
+    all_symlink_firmware_mi8916
+
 # Init
 PRODUCT_COPY_FILES += \
     $(TARGET_DEVICE_PATH)/fstab/fstab.mi8916:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.mi8916 \
