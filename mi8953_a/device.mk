e8d61ef4eef0b4d69d3b4b7f7ed2bc81dc54664f
Yumi Yukimura <me.cafebabe@gmail.com>
2025-07-29 04:03:53 +0800

    mi89xx-mainline: mi8953_a: Enable IR
    Change-Id: I3290be1db8a526158e4a76c709c286286b92c4f4

--- mi8953_a/device.mk
+++ mi8953_a/device.mk
@@ -6,7 +6,7 @@
 TARGET_DEVICE_PATH := device/xiaomi/mi89xx-mainline/mi8953_a
 
 # Inherit options from mainline/qcom-common
-## SoC
+TARGET_HAS_IR := true
 TARGET_QCOM_SOC_FAMILY := msm8953
 ## TODO: Bringup the corresponding hardware and remove the following definitions
 TARGET_AUDIO_HAL := default-aidl
