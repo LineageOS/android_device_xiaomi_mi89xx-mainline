9b9d8a06638f1cee8f3dbbcc5874e9cff4e950dd
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-08-07 16:45:31 +0200

    mi89xx-mainline: Move init and fstab copy rule to Soong
    Also remove mi8953_a dependency on mi89x7, stuff conflicts
    
    In the future commonizing msm8937 and msm8953 should be possible, given
    they do share the BSP downstream
    
    Change-Id: Ic0e08cd09116d79c7b77bc6ab636e792019c9a83

--- mi8916/device.mk
+++ mi8916/device.mk
@@ -28,24 +28,25 @@ PRODUCT_PACKAGES += \
     all_symlink_firmware_mi8916
 
 # Init
-PRODUCT_COPY_FILES += \
-    $(TARGET_DEVICE_PATH)/fstab/fstab.mi8916:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.mi8916 \
-    $(TARGET_DEVICE_PATH)/init/init.mi8916.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.mi8916.rc \
-    $(TARGET_DEVICE_PATH)/init/ueventd.mi8916.rc:$(TARGET_COPY_OUT_VENDOR)/etc/ueventd.mi8916.rc
+PRODUCT_PACKAGES += \
+    fstab.mi8916 \
+    fstab.mi8916.ramdisk \
+    init.mi8916.rc \
+    ueventd.mi8916.rc
 
 PRODUCT_PACKAGES += \
     use_memfd.rc \
     zram.rc
 
 # Input
-PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*.kl,$(TARGET_DEVICE_PATH)/input/,$(TARGET_COPY_OUT_ODM)/usr/keylayout/)
+PRODUCT_PACKAGES += \
+    ts_vkeys.kl
 
 TARGET_TOUCHSCREEN_HAS_VIRTUAL_KEYS := true
 
 # Kernel
-PRODUCT_COPY_FILES += \
-    $(TARGET_DEVICE_PATH)/modprobe/modules.load.normal:$(TARGET_COPY_OUT_VENDOR)/etc/modules.load.normal
+PRODUCT_PACKAGES += \
+    modules.load.normal
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
@@ -54,13 +55,9 @@ DEVICE_PACKAGE_OVERLAYS += \
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITION_SIZE := true
 
-# Ramdisk
-PRODUCT_COPY_FILES += \
-    $(TARGET_DEVICE_PATH)/fstab/fstab.mi8916:$(TARGET_COPY_OUT_RAMDISK)/fstab.mi8916
-
 # Recovery
-PRODUCT_COPY_FILES += \
-    $(TARGET_DEVICE_PATH)/init/init.recovery.mi8916.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.mi8916.rc
+PRODUCT_PACKAGES += \
+    init.recovery.mi8916.rc
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
