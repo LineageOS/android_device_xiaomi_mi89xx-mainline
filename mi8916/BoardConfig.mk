ce58b1a0ec2bc10bc4d1a4e0146a0c62faec8526
Yumi Yukimura <me.cafebabe@gmail.com>
2025-11-09 12:00:28 +0800

    mi89xx-mainline: mi8916: kconfigs: Add config to fixup built-in options
    Test: on current kernel setup, .config matches with b/android-6.12/android-base.config
    Change-Id: I4fd9e8c87d7b389aba9ab2fc7699ec087dd95e9f

--- mi8916/BoardConfig.mk
+++ mi8916/BoardConfig.mk
@@ -30,6 +30,7 @@ TARGET_KERNEL_CONFIG_EXT := \
     kernel/mainline/configs/fragments/y/fbcon.config \
     kernel/mainline/configs/fragments/n/disable-clang-hardening-features.config \
     kernel/mainline/configs/fragments/n/faster-build-time.config \
+    $(TARGET_DEVICE_PATH)/kconfigs/builtin-fixup.config \
     $(TARGET_DEVICE_PATH)/kconfigs/fixups.config
 
 # Kernel modules
