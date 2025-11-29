09d9c920ebb0fe57732d7dd26c7fc69b24131148
Yumi Yukimura <me.cafebabe@gmail.com>
2025-11-30 01:28:08 +0800

    mi89xx-mainline: vendor.prop: Set `persist.sys.zram_enabled=1`
    Change-Id: Ia213f5e3e1c3a4d7837f7c20b5aec8fa47416bbb

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -45,6 +45,7 @@ TARGET_BOARD_PLATFORM := mi89xx
 
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/properties/odm.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/properties/vendor.prop
 
 # Ramdisk
 BOARD_RAMDISK_USE_LZ4 := true
