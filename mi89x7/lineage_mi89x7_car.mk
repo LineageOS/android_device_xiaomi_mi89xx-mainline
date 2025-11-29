deecdfb50374df7657596c7f87ade6e8ccfb369d
Yumi Yukimura <me.cafebabe@gmail.com>
2025-11-30 02:26:06 +0800

    mi89xx-mainline: {mi8953_a,mi89x7}: Add Car product
    Change-Id: Ib617831c41395139794ad3867ad13e34032b7ee9

---
+++ mi89x7/lineage_mi89x7_car.mk
@@ -0,0 +1,23 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, packages/services/Car/car_product/build/car.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, device/lineage/car/lineage_car_vendor.mk)
+$(call inherit-product, vendor/lineage/config/common_car.mk)
+
+# Inherit from device
+PRODUCT_IS_AUTOMOTIVE := true
+$(call inherit-product, device/xiaomi/mi89xx-mainline/mi89x7/device.mk)
+
+PRODUCT_NAME := lineage_mi89x7_car
+PRODUCT_DEVICE := mi89x7_car
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MODEL := MSM89x7 Car
