f4fd0f2ab86673e3fb86ff8dae752587f33cd1f3
Yumi Yukimura <me.cafebabe@gmail.com>
2025-11-30 02:26:06 +0800

    mi89xx-mainline: {mi8953_a,mi89x7}: Add Car product
    Change-Id: Ib617831c41395139794ad3867ad13e34032b7ee9

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -10,7 +10,9 @@ PRODUCT_MAKEFILES := \
     aosp_tiare_mainline:$(LOCAL_DIR)/tiare_mainline/aosp_tiare_mainline.mk \
     lineage_mi8916:$(LOCAL_DIR)/mi8916/lineage_mi8916.mk \
     lineage_mi8953_a:$(LOCAL_DIR)/mi8953_a/lineage_mi8953_a.mk \
+    lineage_mi8953_a_car:$(LOCAL_DIR)/mi8953_a/lineage_mi8953_a_car.mk \
     lineage_mi89x7:$(LOCAL_DIR)/mi89x7/lineage_mi89x7.mk \
+    lineage_mi89x7_car:$(LOCAL_DIR)/mi89x7/lineage_mi89x7_car.mk \
     lineage_tiare_mainline:$(LOCAL_DIR)/tiare_mainline/lineage_tiare_mainline.mk
 
 $(foreach build_type, user userdebug eng, \
@@ -20,5 +22,7 @@ $(foreach build_type, user userdebug eng, \
     $(eval COMMON_LUNCH_CHOICES += aosp_tiare_mainline-$(build_type)) \
     $(eval COMMON_LUNCH_CHOICES += lineage_mi8916-$(build_type)) \
     $(eval COMMON_LUNCH_CHOICES += lineage_mi8953_a-$(build_type)) \
+    $(eval COMMON_LUNCH_CHOICES += lineage_mi8953_a_car-$(build_type)) \
     $(eval COMMON_LUNCH_CHOICES += lineage_mi89x7-$(build_type)) \
+    $(eval COMMON_LUNCH_CHOICES += lineage_mi89x7_car-$(build_type)) \
     $(eval COMMON_LUNCH_CHOICES += lineage_tiare_mainline-$(build_type)))
--- device.mk
+++ device.mk
@@ -35,6 +35,11 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
+ifeq ($(PRODUCT_IS_AUTOMOTIVE),true)
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/car_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/car_core_hardware.xml
+endif
+
 # Set device properties
 PRODUCT_PACKAGES += \
     set_device_prop \
