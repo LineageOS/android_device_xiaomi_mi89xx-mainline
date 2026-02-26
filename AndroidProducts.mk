eac0220b9d676e27bee03c10991b8dbe5417fc15
Yumi Yukimura <me.cafebabe@gmail.com>
2026-02-26 22:42:49 +0800

    [WIP] mi89xx-mainline: mi439_mainline: Initial bringup
    Change-Id: I9eeaf21eb166aeeff8783bba31426e205a348b87

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -4,10 +4,12 @@
 #
 
 PRODUCT_MAKEFILES := \
+    aosp_mi439_mainline:$(LOCAL_DIR)/mi8916/aosp_mi439_mainline.mk \
     aosp_mi8916:$(LOCAL_DIR)/mi8916/aosp_mi8916.mk \
     aosp_mi8953_a:$(LOCAL_DIR)/mi8953_a/aosp_mi8953_a.mk \
     aosp_mi89x7:$(LOCAL_DIR)/mi89x7/aosp_mi89x7.mk \
     aosp_tiare_mainline:$(LOCAL_DIR)/tiare_mainline/aosp_tiare_mainline.mk \
+    lineage_mi439_mainline:$(LOCAL_DIR)/mi89x7/lineage_mi439_mainline.mk \
     lineage_mi8916:$(LOCAL_DIR)/mi8916/lineage_mi8916.mk \
     lineage_mi8953_a:$(LOCAL_DIR)/mi8953_a/lineage_mi8953_a.mk \
     lineage_mi8953_a_car:$(LOCAL_DIR)/mi8953_a/lineage_mi8953_a_car.mk \
@@ -18,10 +20,12 @@ PRODUCT_MAKEFILES := \
     lineage_tiare_mainline:$(LOCAL_DIR)/tiare_mainline/lineage_tiare_mainline.mk
 
 $(foreach build_type, user userdebug eng, \
+    $(eval COMMON_LUNCH_CHOICES += aosp_mi439_mainline-$(build_type)) \
     $(eval COMMON_LUNCH_CHOICES += aosp_mi8916-$(build_type)) \
     $(eval COMMON_LUNCH_CHOICES += aosp_mi8953_a-$(build_type)) \
     $(eval COMMON_LUNCH_CHOICES += aosp_mi89x7-$(build_type)) \
     $(eval COMMON_LUNCH_CHOICES += aosp_tiare_mainline-$(build_type)) \
+    $(eval COMMON_LUNCH_CHOICES += lineage_mi439_mainline-$(build_type)) \
     $(eval COMMON_LUNCH_CHOICES += lineage_mi8916-$(build_type)) \
     $(eval COMMON_LUNCH_CHOICES += lineage_mi8953_a-$(build_type)) \
     $(eval COMMON_LUNCH_CHOICES += lineage_mi8953_a_car-$(build_type)) \
