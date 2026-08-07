d7727701a1f64baebbdecaa7a6755716072b24bb
Yumi Yukimura <me.cafebabe@gmail.com>
2026-08-07 19:20:45 +0800

    mi89xx-mainline: Add `mi8956`
    Change-Id: Id8234b5139e8357fc6f1df794ca60493ea5f08f1

---
+++ mi8956/BoardConfig.mk
@@ -0,0 +1,64 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from parent
+include device/xiaomi/mi89xx-mainline/BoardConfig.mk
+
+# A/B
+AB_OTA_UPDATER := false
+
+# Boot parameters
+BOARD_KERNEL_CMDLINE += \
+    androidboot.hardware=mi8956
+
+# Kernel
+TARGET_KERNEL_SOURCE := kernel/mainline/msm8956-mainline
+
+TARGET_DTB_LIST_WILDCARD := qcom/msm8956-xiaomi-*
+
+TARGET_KERNEL_CONFIG := msm8956_defconfig
+TARGET_KERNEL_CONFIG_EXT := \
+    kernel/mainline/configs/fragments/android-base-pre/common.config \
+    kernel/mainline/configs/fragments/android-base-pre/arm64.config \
+    kernel/configs/b/android-6.12/android-base.config \
+    kernel/mainline/configs/fragments/android-base-conditional/CONFIG_ARM64-y.config \
+    kernel/mainline/configs/fragments/common.config \
+    kernel/mainline/configs/fragments/y/fbcon.config \
+    kernel/mainline/configs/fragments/n/disable-clang-hardening-features.config \
+    kernel/mainline/configs/fragments/n/faster-build-time.config
+
+# Kernel modules
+BOARD_RECOVERY_RAMDISK_KERNEL_MODULES_LOAD := \
+    $(strip $(shell cat $(TARGET_DEVICE_PATH)/modprobe/modules.load.basic)) \
+    $(strip $(shell cat $(TARGET_DEVICE_PATH)/modprobe/modules.load.drm)) \
+    $(strip $(shell cat $(TARGET_DEVICE_PATH)/modprobe/modules.load.power_supply)) \
+    $(strip $(shell cat $(TARGET_DEVICE_PATH)/modprobe/modules.load.touchscreen))
+BOARD_VENDOR_KERNEL_MODULES_LOAD := \
+    $(BOARD_RECOVERY_RAMDISK_KERNEL_MODULES_LOAD)
+RECOVERY_KERNEL_MODULES := \
+    $(BOARD_RECOVERY_RAMDISK_KERNEL_MODULES_LOAD)
+
+TARGET_AUTO_COLLECT_KERNEL_MODULE_DEPS := true
+
+# OTA
+TARGET_OTA_ASSERT_DEVICE := mi8956
+
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
+BOARD_CACHEIMAGE_PARTITION_SIZE := 268435456
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
+BOARD_SYSTEMIMAGE_EXTFS_INODE_COUNT := -1
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2684354560
+BOARD_VENDORIMAGE_EXTFS_INODE_COUNT := -1
+BOARD_VENDORIMAGE_PARTITION_SIZE := 536870912
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+
+BOARD_USES_METADATA_PARTITION := true
+TARGET_COPY_OUT_VENDOR := vendor
+
+# Recovery
+TARGET_RECOVERY_DENSITY := xxhdpi
+TARGET_RECOVERY_FSTAB := $(TARGET_DEVICE_PATH)/fstab/fstab.mi8956
