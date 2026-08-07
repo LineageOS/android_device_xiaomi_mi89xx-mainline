b10fe423cadfaf819a5583bb7bc95882aa47f906
Yumi Yukimura <me.cafebabe@gmail.com>
2026-08-07 19:20:45 +0800

    mi89xx-mainline: Add `mi8956`
    Change-Id: Id8234b5139e8357fc6f1df794ca60493ea5f08f1

---
+++ mi8956/device.mk
@@ -0,0 +1,56 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+TARGET_DEVICE_PATH := device/xiaomi/mi89xx-mainline/mi8956
+
+# Inherit options from mainline/qcom-common
+TARGET_HAS_IR := true
+TARGET_QCOM_SOC := msm8956
+## TODO: Bringup the corresponding hardware and remove the following definitions
+TARGET_SUPPORTS_SUSPEND := false
+include device/mainline/qcom-common/optional/options.mk
+
+# Inherit from parent
+$(call inherit-product, device/xiaomi/mi89xx-mainline/device.mk)
+
+# AAPT
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
+# Audio
+PRODUCT_PACKAGES += \
+    audio.mi8956.xml
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# Dalvik heap
+$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+
+# Init
+PRODUCT_PACKAGES += \
+    fstab.mi8956 \
+    fstab.mi8956.ramdisk \
+    init.mi8956.rc \
+    init.recovery.mi8956.rc \
+    ueventd.mi8956.rc
+
+PRODUCT_PACKAGES += \
+    use_memfd.rc
+
+$(call soong_config_set,mainline_common_libinit,set_properties_from,devicetree)
+
+# Kernel
+PRODUCT_PACKAGES += \
+    modules.load.normal
+
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(TARGET_DEVICE_PATH)/overlays/overlay
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(TARGET_DEVICE_PATH) \
+    kernel/mainline/configs
